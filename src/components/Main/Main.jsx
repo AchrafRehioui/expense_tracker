91621116a3beadd52e752404009fb4d54c939fd1
Achraf <achraf.rehioui.1@gmail.com>
2021-01-17 18:18:39 +0100

    adding Form component in Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
 import useStyles from './styles';
+import Form from './Form/Form';
 
 const Main = () => {
     const classes = useStyles();
@@ -15,11 +16,12 @@ const Main = () => {
                     Try saying: Add income for $100 in category salary for Monday
                 </Typography>
                 <Divider />
-                {/* Form */}
+                <Form />
             </CardContent>
             <CardContent className={classes.cardContent}>
                 <Grid container spacing={2}>
                         <Grid item xs={12}>
+                            {/*<List /> */}
                         </Grid>
                 </Grid>
             </CardContent>
