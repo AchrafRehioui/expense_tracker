b8d3dd359b68a8a036df095fcbe82cae00de3f89
Achraf <achraf.rehioui.1@gmail.com>
2021-01-19 11:32:49 +0100

    adding formatDate

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react';
 import { TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem } from '@material-ui/core';
 import { ExpenseTrackerContext } from '../../../context/context';
 import { v4 as uuidv4 } from 'uuid';
+import formatDate from '../../../utils/formatDate';
 import useStyles from './styles';
 import { incomeCategories, expenseCategories } from '../../../constants/categories';
 
@@ -10,7 +11,7 @@ const initialState = {
     amount: '',
     category: '',
     type: 'Income',
-    date: new Date(),
+    date: formatDate(new Date()),
 }
 
 const Form = () => {
@@ -56,9 +57,9 @@ const Form = () => {
                 <TextField type="number" label="Amount" fullWidth value={formData.amount} onChange={(e) => setFormData({ ...formData, amount: e.target.value })}/>
             </Grid>
             <Grid item xs={6}>
-                <TextField type="date" label="Date" fullWidth value={formData.date}  onChange={(e) => setFormData({ ...formData, date: e.target.value })}/>
+                <TextField type="date" label="Date" fullWidth value={formData.date}  onChange={(e) => setFormData({ ...formData, date: formatDate(e.target.value) })}/>
             </Grid>
-            <Button className={classes.button} variant="outlined" color="primary" fullWidth  onClick={createTransaction}>Create</Button>
+            <Button className={classes.button} variant="outlined" color="primary" fullWidth onClick={createTransaction}>Create</Button>
         </Grid>
     )
 }
