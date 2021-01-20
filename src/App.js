ef7049cee9d26f11dabd2ec8b7f8249d318c3f33
Achraf <achraf.rehioui.1@gmail.com>
2021-01-20 17:54:03 +0100

    adding segment in form

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
-import { PushToTalkButton, PushToTalkButtonContainer  } from '@speechly/react-ui';
+import { PushToTalkButton, PushToTalkButtonContainer, ErrorPanel  } from '@speechly/react-ui';
 import Details from './components/Details/Details';
 import Main from './components/Main/Main';
 import useStyles from './styles';
@@ -21,10 +21,11 @@ const App = () => {
                     <Grid item xs={12} sm={4}>
                         <Details title="Expense" />
                     </Grid>
+            </Grid>
                     <PushToTalkButtonContainer>
                         <PushToTalkButton />
+                        <ErrorPanel />
                     </PushToTalkButtonContainer>
-            </Grid>
         </div>
     )
 }
